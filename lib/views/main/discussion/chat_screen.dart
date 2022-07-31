5e27ec86959036a4de31ca26620e94febc919621
mahesawp45 <kt.mahesa@gmail.com>
2022-07-31 13:06:47 +0800

    Adding and Getting Chat from Firebase Firestore

--- lib/views/main/discussion/chat_screen.dart
+++ lib/views/main/discussion/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:latihan_soal_app/constants/r.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -12,10 +13,19 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   TextEditingController textEditingController = TextEditingController();
-  CollectionReference chat = FirebaseFirestore.instance
-      .collection("room")
-      .doc("kimia")
-      .collection("chat");
+  late CollectionReference chat;
+  final user = FirebaseAuth.instance.currentUser!;
+  final listViewController = ScrollController();
+
+  @override
+  void initState() {
+    chat = FirebaseFirestore.instance
+        .collection("room")
+        .doc("kimia")
+        .collection("chat");
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,48 +36,99 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: 1,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Nama User',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Color(0xff5200ff),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 4),
-                          decoration: BoxDecoration(
-                            color: Colors.blue.withOpacity(0.1),
-                            borderRadius: const BorderRadius.only(
-                              bottomLeft: Radius.circular(20),
-                              bottomRight: Radius.circular(20),
-                              topRight: Radius.circular(20),
+            child: StreamBuilder(
+              stream: chat.orderBy('sent', descending: true).snapshots(),
+              builder:
+                  (context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                final listChat = snapshot.data?.docs;
+                return ListView.builder(
+                  reverse: true,
+                  controller: listViewController,
+                  itemCount: listChat?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final currentChat = snapshot.data?.docs[index];
+                    final currentDate =
+                        (currentChat?['sent'] as Timestamp?)?.toDate();
+
+                    return Container(
+                      margin: user.uid == currentChat?['uid']
+                          ? const EdgeInsets.only(bottom: 15, right: 15)
+                          : const EdgeInsets.only(bottom: 15, left: 15),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Column(
+                        crossAxisAlignment: user.uid == currentChat?['uid']
+                            ? CrossAxisAlignment.end
+                            : CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            currentChat?['name'] ?? '',
+                            style: const TextStyle(
+                              fontSize: 10,
+                              color: Color(0xff5200ff),
                             ),
                           ),
-                          child: const Text('Isi Pesan'),
-                        ),
-                        Text(
-                          'Waktu kirim',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: R.appCOLORS.greySubtitleColor,
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 4),
+                            decoration: BoxDecoration(
+                              color: user.uid == currentChat?['uid']
+                                  ? R.appCOLORS.primaryColor
+                                  : Colors.pink.withOpacity(0.1),
+                              borderRadius: user.uid == currentChat?['uid']
+                                  ? const BorderRadius.only(
+                                      bottomLeft: Radius.circular(20),
+                                      topLeft: Radius.circular(20),
+                                      topRight: Radius.circular(20),
+                                    )
+                                  : const BorderRadius.only(
+                                      bottomLeft: Radius.circular(20),
+                                      bottomRight: Radius.circular(20),
+                                      topRight: Radius.circular(20),
+                                    ),
+                            ),
+                            child: Text(
+                              currentChat?['content'] ?? '',
+                              style: TextStyle(
+                                color: user.uid == currentChat?['uid']
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+                          Text(
+                            currentDate?.day == (DateTime.now().day - 1)
+                                ? "Yesterday at ${currentDate?.hour ?? ''}:${currentDate?.minute ?? ''}" //Kemarin
+                                : currentDate?.hour == DateTime.now().hour
+                                    ? (DateTime.now().minute -
+                                                (currentDate ?? DateTime.now())
+                                                    .minute) ==
+                                            0
+                                        ? 'now' // saat ini juga
+                                        : '${DateTime.now().minute - (currentDate ?? DateTime.now()).minute}m' // sebelum 1 jam hari ini
+                                    : currentDate?.day !=
+                                                (DateTime.now().day - 1) &&
+                                            currentDate?.month !=
+                                                (DateTime.now().month)
+                                        ? DateFormat('dd MMM yyy, HH:mm')
+                                            .format(currentDate ??
+                                                DateTime
+                                                    .now()) // 2 hari sebelum hari ini
+                                        : "${currentDate?.hour ?? ''}:${currentDate?.minute ?? ''}", // Hari ini
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: R.appCOLORS.greySubtitleColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
           SafeArea(
@@ -107,6 +168,7 @@ class _ChatScreenState extends State<ChatScreen> {
                             child: TextField(
                               keyboardType: TextInputType.multiline,
                               controller: textEditingController,
+                              cursorColor: R.appCOLORS.primaryColor,
                               decoration: const InputDecoration(
                                   contentPadding: EdgeInsets.only(right: 10),
                                   border: InputBorder.none,
@@ -134,7 +196,6 @@ class _ChatScreenState extends State<ChatScreen> {
                         if (textEditingController.text.isEmpty) {
                           return;
                         }
-                        final user = FirebaseAuth.instance.currentUser!;
 
                         final chatContent = {
                           'uid': user.uid,
@@ -146,9 +207,14 @@ class _ChatScreenState extends State<ChatScreen> {
                         };
 
                         // Masukkin data ke Firebase Store
-                        chat.add(chatContent);
-
-                        textEditingController.text = '';
+                        // chat.add(chatContent).whenComplete(() {
+                        //   getDataFromFirebase(); // Sementara untuk stream datanya bisa bgini
+                        // });
+                        chat.add(chatContent).whenComplete(
+                          () {
+                            textEditingController.clear();
+                          },
+                        );
                       },
                       icon: Icon(
                         Icons.send,
