83f3e14e92682cab52d1e9e43d596eb1bebd6162
mahesawp <kt.mahesa@gmail.com>
2022-07-31 13:49:02 +0800

    API endpoints

---
+++ lib/constants/api_url.dart
@@ -0,0 +1,16 @@
+class ApiUrl {
+  static String baseURL = 'https://ekskul.id/api/';
+
+  // endpoint
+  // user
+  static String users = "users";
+  static String userRegistration = "users/registrasi";
+  static String userUpdateProfile = "users/update";
+
+  // Latihan Soal
+  static String latihanMapel = "exercise/data_course";
+  static String latihanPaketSoal = "exercise/data_course";
+  static String latihanKerjakanSoal = "/exercise/kerjakan";
+  static String latihanSubmitJawaban = "/exercise/input_jawaban";
+  static String latihanSkor = "/exercise/score_result";
+}
